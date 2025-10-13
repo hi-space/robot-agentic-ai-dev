c4a001d0266db881444a48281206211d20e8d431
Sejin Kim <sejjj120@gmail.com>
2025-10-14 08:55:43 +0900

    feat: Add S3 get permission

--- amplify-frontend/amplify/backend.ts
+++ amplify-frontend/amplify/backend.ts
@@ -16,11 +16,23 @@ backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
   new PolicyStatement({
     actions: [
       'polly:SynthesizeSpeech',
-      'lambda:InvokeFunction'
+      'lambda:InvokeFunction',
     ],
     resources: [
       '*',
-      'arn:aws:lambda:ap-northeast-2:533267442321:function:lambda-robo-controller-for-robo'
+      'arn:aws:lambda:ap-northeast-2:533267442321:function:lambda-robo-controller-for-robo',
+    ],
+  })
+);
+
+// Add S3 permissions for robot detection images
+backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
+  new PolicyStatement({
+    actions: [
+      's3:GetObject',
+    ],
+    resources: [
+      'arn:aws:s3:::industry-robot-detected-images/*',
     ],
   })
 );
